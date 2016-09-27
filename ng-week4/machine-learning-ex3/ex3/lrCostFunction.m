e8f3681a695b7d38ed8c83f08bf4d2a655d165e8
Cody Heimberger <certifiedloud@gmail.com>
2016-09-27 16:58:26 -0600

    still trying to figure out what I need to do with this

--- ng-week4/machine-learning-ex3/ex3/lrCostFunction.m
+++ ng-week4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -37,16 +37,17 @@ grad = zeros(size(theta));
 %
 
 
+J = ( (1/m) * sum(-y .* log(sigmoid(X*theta)) - (1-y).*log(1-sigmoid(X*theta)))) + (lambda/(2*m))*(sum(theta(2:length(theta)) .^ 2));
+
+grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
 
-
-
-
+grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
 
 
 
 % =============================================================
 
-grad = grad(:);
+%grad = grad(:);
 
 end
